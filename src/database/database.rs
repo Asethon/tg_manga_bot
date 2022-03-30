98cc2b738353e5d0d6e867609eac1457caa5d998
Asethon <vlp.tiberium@gmail.com>
2022-03-30 21:33:06 +0300

    Fix

--- src/database/database.rs
+++ src/database/database.rs
@@ -1,4 +1,4 @@
-use postgres::{Client, NoTls};
+use tokio_postgres::{Client, NoTls};
 
 pub struct DatabaseConnection {
     client: Client,
@@ -10,8 +10,12 @@ impl DatabaseConnection {
         let password = dotenv::var("POSTGRES_PASSWORD").unwrap();
         let host = dotenv::var("POSTGRES_HOST").unwrap();
         let params = format!("postgresql://{}:{}@{}", login, password, host);
-        let client = Client::connect(&params, NoTls);
-
+        let (client, connect) = tokio_postgres::connect(&*params, NoTls);
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
         client.unwrap()
     }
 }
