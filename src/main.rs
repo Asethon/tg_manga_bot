fc7577b8ce16a29178863ab8da4ae0e036556124
Asethon <vlp.tiberium@gmail.com>
2022-04-02 20:00:32 +0300

    Some shit

--- src/main.rs
+++ src/main.rs
@@ -245,11 +245,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let bot = Bot::from_env().auto_send();
 
     let handler = dptree::entry()
-        .branch(Update::filter_message()
-            .enter_dialogue::<Message, InMemStorage<State>, State>()
-            .dispatch_by::<State>()
+        .branch(
+            Update::filter_message()
+                .branch(
+                    dptree::entry()
+                        .enter_dialogue::<Message, InMemStorage<State>, State>()
+                        .dispatch_by::<State>()
+                )
+                .endpoint(message_handler)
         )
-        .branch(Update::filter_callback_query().endpoint(callback_handler));
+        .branch(
+            Update::filter_callback_query()
+                .branch(
+                    dptree::entry()
+                        .enter_dialogue::<CallbackQuery, InMemStorage<State>, State>()
+                        .dispatch_by::<State>()
+                )
+                .endpoint(callback_handler)
+        );
 
     Dispatcher::builder(bot, handler)
         .dependencies(dptree::deps![InMemStorage::<State>::new()])
