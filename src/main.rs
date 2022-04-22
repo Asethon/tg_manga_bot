2df2dd730647e9baa0f576d56317d7a39dcdf3df
Asethon <vlp.tiberium@gmail.com>
2022-04-22 13:01:24 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -321,25 +321,26 @@ async fn main() {
                 .endpoint(callback_handler)
         )
         .branch(
-                teloxide::handler![State::AddBookTitle]
-                    .endpoint(add_book_title_handler)
-            )
-            .branch(
-                teloxide::handler![State::AddBookType { title }]
-                    .endpoint(add_book_type_handler)
-            )
-            .branch(
-                teloxide::handler![State::AddBookDescription { title, book_type }]
-                    .endpoint(add_book_description_handler)
-            )
-            .branch(
-                teloxide::handler![State::AddChapterId { book_id }]
-                    .endpoint(add_chapter_id_handler)
-            )
-            .branch(
-                teloxide::handler![State::AddChapterLink { book_id, chapter_id }]
-                    .endpoint(add_chapter_link_handler)
-            )
+            teloxide::handler![State::AddBookTitle]
+                .endpoint(add_book_title_handler)
+        )
+        .branch(
+            teloxide::handler![State::AddBookType { title }]
+                .endpoint(add_book_type_handler)
+        )
+        .branch(
+            teloxide::handler![State::AddBookDescription { title, book_type }]
+                .endpoint(add_book_description_handler)
+        )
+        .branch(
+            teloxide::handler![State::AddChapterId { book_id }]
+                .endpoint(add_chapter_id_handler)
+        )
+        .branch(
+            teloxide::handler![State::AddChapterLink { book_id, chapter_id }]
+                .endpoint(add_chapter_link_handler)
+        )
+        .enter_dialogue::<Update, InMemStorage<State>, State>()
         ;
 
     Dispatcher::builder(bot.clone(), handler)
